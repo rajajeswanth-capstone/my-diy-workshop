b7ef17bcf5cd84c239caa9b4d1d5e1cc7d0ccc9a
Raja Jeswanth <rajajeswanth.capstone@outlook.com>
2024-08-20 18:35:49 -0500

    initial code commit

--- scripts/mac/upgrade.sh
+++ scripts/mac/upgrade.sh
@@ -5,3 +5,16 @@ for file in *.jar; do
 done
 upgrade_version=0.0.$(($version + 1))
 echo $upgrade_version
+upgrade_directory=diy-workshop-$upgrade_version-release
+upgrade_file=diy-workshop-$upgrade_version-release.zip
+echo $upgrade_directory
+url=https://github.com/rajajeswanth-capstone/my-diy-workshop/blob/main/releases/diy-workshop-$upgrade_version-release.zip
+if curl --output /dev/null --silent --head --fail "$url"; then
+  curl -O $url
+  unzip $upgrade_file
+  rm -rf *.jar
+  cp $upgrade_directory/$upgrade_file .
+  rm -rf $upgrade_directory
+else
+  echo "Already up-to-date"
+fi
